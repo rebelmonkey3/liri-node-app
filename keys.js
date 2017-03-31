4dedbf4c27be555ffca03ff1089fa40276461b9b
Blake Weisgarber <blakeweisgarber@Blakes-MacBook-Pro.local>
2017-03-30 18:33:10 -0700

    Spotify working, Orlando Helped

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'SeCzNmBRVWv118RuDo4s9MTxd',
+  consumer_secret: 'Dg3CoJbvkCyHFlAUtDQqEi7YPlK5DLcjoO2WHveCHUJ3InURu7',
+  access_token_key: '2515549957-G0kDK2YifuYz9ljiHzlhvwePGQJVHdCU7ai3b6l',
+  access_token_secret: '6yLq9ureWcEMUTPxT2JMR922puiiyqYKHedViwKYuGBKc',
+}
---
+++ liri.js
@@ -0,0 +1,89 @@
+
+// write the code you need to grab the data from keys.js. Then store the keys in a variable.
+
+var sourceFile = require('./keys.js');
+console.log(sourceFile.twitterKeys);
+
+var request = require('request')
+var fs = require('fs');
+var question = process.argv[2];
+
+//  Make it so liri.js can take in one of the following commands:
+// * `my-tweets`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says` 
+
+var Twitter = require('twitter');
+
+	//Twtitter section-
+
+if(question === "my-tweets"){
+	 
+		var client = new Twitter(sourceFile.twitterKeys);
+		 
+		var params = {screen_name: 'nodejs'};
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		  if (!error) {
+		    console.log(tweets);
+			}
+		});
+}
+	//end of Twitter section
+
+
+	var spotify = require('spotify')
+
+	//Spotify Section
+
+if(question === "spotify-this-song"){
+    var songTitle = process.argv[3];
+    spotify.search({ type: 'track', query: songTitle }, function(err, data){
+        
+        if(process.argv[3]){
+            var data = data.tracks.items;
+            for(var i =0; i < data.length; i++){
+                
+                console.log(data[i].name); //song track name
+                console.log(data[i].album.href); //url 
+                console.log(data[i].album.name); //album name
+                console.log(data[i].preview_url); //preview link to the song
+            
+                for(var j =0; j < data[i].artists.length; j++){
+                    console.log(data[i].artists[j].name); //artist's name
+                }
+            }
+        }else{
+            spotify.search({ type: 'track', query: "what's my age again"}, function(err, data){
+                var data = data.tracks.items;
+                console.log(data[0].name); //song track name
+                console.log(data[0].album.href); //url 
+                console.log(data[0].album.name); //album name
+                console.log(data[0].preview_url); //preview link to the song
+                console.log(data[0].artists[0].name); //artist's name
+            });
+        }
+    });
+    outputText();
+}
+	//End of Spotify section
+
+
+// var omdb = require('omdb');
+
+	//OMDB section
+
+if(question === "movie-this"){	
+
+
+}
+	//end of OMDB
+
+
+function outputText(){
+        console.log(spotify.response)
+    }
+	
